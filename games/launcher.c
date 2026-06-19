8587ad6ababc90aa533daeaa1150e0c2dcbac23b
Sujay Paul <sujaypaul892@gmail.com>
2026-06-19 22:15:24 +0530

    Fix launcher termination by making GTK applications non-unique

--- games/launcher.c
+++ games/launcher.c
@@ -417,7 +417,7 @@ static void activate(GtkApplication *app, gpointer user_data)
 
 int main(int argc, char **argv)
 {
-    GtkApplication *app = gtk_application_new("org.sujay.gameslauncher", G_APPLICATION_DEFAULT_FLAGS);
+    GtkApplication *app = gtk_application_new("org.sujay.gameslauncher", G_APPLICATION_NON_UNIQUE);
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
     int status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
--- games/number-guessing/main.c
+++ games/number-guessing/main.c
@@ -576,7 +576,7 @@ static void activate(GtkApplication *app_system, gpointer user_data)
 // --- PROGRAM ENTRY POINT ---
 int main(int argc, char **argv)
 {
-    GtkApplication *app = gtk_application_new("org.sujay.numberguess", G_APPLICATION_DEFAULT_FLAGS);
+    GtkApplication *app = gtk_application_new("org.sujay.numberguess", G_APPLICATION_NON_UNIQUE);
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
     int status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
